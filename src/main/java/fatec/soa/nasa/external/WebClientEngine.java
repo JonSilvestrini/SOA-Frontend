244faa1d4a93f5de3b03633a4a114aa00edfc870
JonSilvestrini <jonatas.silvestrini@gmail.com>
2022-11-29 19:00:39 -0300

    correção token

--- src/main/java/fatec/soa/nasa/external/WebClientEngine.java
+++ src/main/java/fatec/soa/nasa/external/WebClientEngine.java
@@ -1,20 +1,22 @@
 package fatec.soa.nasa.external;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.reactive.function.client.WebClient;
 
+@Configuration
 public class WebClientEngine<T> {
 
-	@Value("${api.key}")
-	private String token;
+	@Value("${api.token}")
+	private String apiToken;
 
     private final WebClient client = WebClient.create();
 
     public T callUrlSync(String url, ParameterizedTypeReference<T> typeReference) {
         return client.get()
                 .uri(url)
-                .header("Authorization", token)
+                .header("Authorization", apiToken)
                 .retrieve()
                 .bodyToMono(typeReference)
                 .block();
--- src/main/java/fatec/soa/nasa/services/IsAliveService.java
+++ src/main/java/fatec/soa/nasa/services/IsAliveService.java
@@ -1,5 +1,6 @@
 package fatec.soa.nasa.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +10,8 @@ import fatec.soa.nasa.external.WebClientEngine;
 @Service
 public class IsAliveService {
 
-	private WebClientEngine<IsAliveDTO> webClient = new WebClientEngine<>();
+	@Autowired
+	private WebClientEngine<IsAliveDTO> webClient;
 
 	public IsAliveDTO getIsAlive() {
 		return webClient.callUrlSync("http://168.138.133.60:9090/isalive",
