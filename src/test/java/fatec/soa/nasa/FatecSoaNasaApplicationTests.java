38cf08d7f4c370c3adf2d24ff1b7f03c65d1e7bf
JonSilvestrini <jonatas.silvestrini@gmail.com>
2022-11-29 22:10:26 -0300

    correção teste

--- src/test/java/fatec/soa/nasa/FatecSoaNasaApplicationTests.java
+++ src/test/java/fatec/soa/nasa/FatecSoaNasaApplicationTests.java
@@ -1,13 +1,8 @@
-package fatec_soa_nasa.fatec_soa_nasa;
+package fatec.soa.nasa;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FatecSoaNasaApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
