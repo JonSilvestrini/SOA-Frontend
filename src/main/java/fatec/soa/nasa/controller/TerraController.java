59a3fa644f76eb4f9a84fbc1258c7c8c495bae3b
JonSilvestrini <jonatas.silvestrini@gmail.com>
2022-11-30 22:48:52 -0300

    terra data pickup

--- src/main/java/fatec/soa/nasa/controller/TerraController.java
+++ src/main/java/fatec/soa/nasa/controller/TerraController.java
@@ -3,11 +3,15 @@ package fatec.soa.nasa.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import fatec.soa.nasa.dto.request.terra.TerraImagemDTO;
 import fatec.soa.nasa.services.TerraService;
@@ -18,13 +22,27 @@ public class TerraController {
 	@Autowired
 	private TerraService terraService;
 
-    @GetMapping("/terra/{date}")
-    public String terra(@PathVariable("date") String date, ModelMap model){
-    	List<TerraImagemDTO> terraImagem = terraService.getPlanetaVisivel(date);
+	@GetMapping("/terra")
+	public String terra(@RequestParam(name = "data", required = false) String date, ModelMap model) {
+		if (date != null && !date.isBlank()) {
+			try {
+				List<TerraImagemDTO> terraImagem = terraService.getPlanetaVisivel(date);
 
-    	model.addAttribute("terraImagem", terraImagem);
+				model.addAttribute("terraImagem", terraImagem);
+				model.addAttribute("data", date);
 
+				return "paginas/terraview";
+			} catch (WebClientResponseException e) {
+				if (e.getStatusCode() == HttpStatus.NOT_FOUND) {
+					model.addAttribute("data", date);
+					return "paginas/terra_404";
+				}
+				return "paginas/terra";
+			}
+		} else {
 
-        return "paginas/terra";
-    }
+			return "paginas/terra";
+		}
+	}
+
 }
