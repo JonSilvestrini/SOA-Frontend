3e49b0433fd14a8bd8e65e8efb8e538d1be530e9
JonSilvestrini <jonatas.silvestrini@gmail.com>
2022-12-03 22:39:10 -0300

    index e server port

--- src/main/java/fatec/soa/nasa/controller/IndexController.java
+++ src/main/java/fatec/soa/nasa/controller/IndexController.java
@@ -1,31 +1,17 @@
 package fatec.soa.nasa.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import fatec.soa.nasa.dto.request.IsAliveDTO;
-import fatec.soa.nasa.services.IsAliveService;
-
 @RequestMapping("/")
 @Controller
 public class IndexController {
 
-	@Autowired
-	private IsAliveService isAliveService;
-
     @GetMapping
     public String index(ModelMap model){
 
-    	IsAliveDTO isAlive = isAliveService.getIsAlive();
-
-    	model.addAttribute("isAlive", isAlive);
-
-    	System.out.println(isAlive.toString());
-
-        System.out.println("Passou pelo Controller Index...");
         return "index";
     }
 
